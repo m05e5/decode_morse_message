a4d018222515bb55c07700a5faebf2430a6f716d
m05e5 <calebmoses1111@gmail.com>
2021-10-06 17:34:18 +0100

    fix linters

--- index.rb
+++ index.rb
@@ -28,36 +28,33 @@ $letters = {
 }
 
 def decode_char(character)
-  $letters.each {
-    |key,value|
+  $letters.each do |key,value|
     if value == character
       return key
       break
     end
-  }
+  end
 end
 
 def decode_word(word)
   word_array = word.split
   result = ''
-  word_array.each {
-  |i|
+  word_array.each do |i|
     result = result + decode_char(i).to_s
-  }
-  return result
+  end
+  print result
 end
-    
+
 def decode_phrase(phrase)
-    new_phrase = phrase.gsub('/', ' ')
+  new_phrase = phrase.gsub('/', ' ')
     phrase_array = new_phrase.split(/   /, -1)
     final = ''
-    phrase_array.each {
-      |j|
-      if final == ''
-        final = decode_word(j).to_s
+    phrase_array.each do |j|
+      final = if final == ''
+        decode_word(j).to_s
       else
-        final = final + ' ' + decode_word(j).to_s
+        '#{final} #{decode_word(j).to_s}'
       end
-    }
+    end
     print final
 end
