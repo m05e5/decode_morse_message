cff1d20896317872ecdcca037793f5c105f82427
Sylvestre <pasytchangwa@gmail.com>
2021-10-06 19:50:39 +0300

    Fix linters

--- index.rb
+++ index.rb
@@ -1,34 +1,34 @@
 $letters = {
-    A: '.-',
-    B: '-...',
-    C: '-.-.',
-    D: '-..',
-    E: '.',
-    F: '..-.',
-    G: '--.',
-    H: '....',
-    I: '..',
-    J: '.---',
-    K: '-.-',
-    L: '.-..',
-    M: '--',
-    N: '-.',
-    O: '---',
-    P: '.--.',
-    Q: '--.-',
-    R: '.-.',
-    S: '...',
-    T: '-',
-    U: '..-',
-    V: '...-',
-    W: '.--',
-    X: '-..-',
-    Y: '-.--',
-    Z: '--..'
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..'
 }
 
 def decode_char(character)
-  $letters.each do |key,value|
+  $letters.each do |key, value|
     if value == character
       return key
       break
@@ -40,21 +40,21 @@ def decode_word(word)
   word_array = word.split
   result = ''
   word_array.each do |i|
-    result = result + decode_char(i).to_s
+    result += decode_char(i).to_s
   end
   print result
 end
 
 def decode_phrase(phrase)
   new_phrase = phrase.gsub('/', ' ')
-    phrase_array = new_phrase.split(/   /, -1)
-    final = ''
-    phrase_array.each do |j|
-      final = if final == ''
-        decode_word(j).to_s
-              else
-        "#{final} #{decode_word(j).to_s}"
-              end
-    end
-    print final
+  phrase_array = new_phrase.split(/   /, -1)
+  final = ''
+  phrase_array.each do |j|
+    final = if final == ''
+              decode_word(j).to_s
+            else
+              "#{final} #{decode_word(j)}.to_s"
+            end
+  end
+  print final
 end
